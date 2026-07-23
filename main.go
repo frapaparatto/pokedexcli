d87e8e0f8dbaf0b5e1c246c9b754a562f3fa332d
Francesco Paparatto <francescopaparatto@gmail.com>
2026-07-23 18:39:59 +0200

    Add basic REPL loop with input cleaning and exit command
    Introduces a command map, input tokenizer, and stdin read loop as the
    foundation for the Pokedex CLI, plus a test for input normalization.

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommands struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func cleanInput(text string) []string {
+	return strings.Fields(strings.ToLower(text))
+}
+
+func commandExit() error {
+	// TODO: to be completed
+	return nil
+}
+
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	commands := map[string]cliCommands{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		word := cleanInput(input)[0]
+		fmt.Println("Your command was:", word)
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+	}
 }
