aa210e072e5d6b534bc4ddd3fe93c5d5b611de3b
Francesco Paparatto <francescopaparatto@gmail.com>
2026-08-13 09:32:58 +0200

    feat: implement cache reaping and wire cache into main
    - Rename pokecache/cache.go -> pokecache.go (and test file) with an
      actual reapLoop implementation that evicts expired entries
    - Instantiate the cache in main.go
    - Ignore generated output.json

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -42,4 +42,19 @@ func NewCache(interval time.Duration) Cache {
 	return c
 }
 
-func (c *Cache) reapLoop(interval time.Duration) {}
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
+
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	for k, v := range c.cache {
+		if v.createdAt.Before(now.Add(-last)) {
+			delete(c.cache, k)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "github.com/frapaparatto/pokedexcli/internal/repl"
+import (
+	"time"
+
+	"github.com/frapaparatto/pokedexcli/internal/pokecache"
+	"github.com/frapaparatto/pokedexcli/internal/repl"
+)
 
 func main() {
+	cache := pokecache.NewCache(5 * time.Millisecond)
+
 	conf := repl.NewConfig()
 	conf.Next = "https://pokeapi.co/api/v2/location-area/"
 	repl.Start(conf)
